c9d784b21165fc16697215fa86c069acdf34b1f0
Pawsome-Sky <skaistediskiene@gmail.com>
2024-08-08 20:42:15 +0300

    Code for check

--- Student_Grade_Tracker.py
+++ Student_Grade_Tracker.py
@@ -10,4 +10,11 @@ while i < num_students:
     i += 1
     for student in students:
         for subject in subjects:
-            grade = int(input(f"Enter the {subject} subject's grade of {student}: "))
+            while True:
+                grade = int(input(f"Enter the grade {student} received in the {subject} subject: "))
+                if 0 <= grade <= 100:
+                    break
+                else:
+                    print("Please enter a valid grade between 0 and 100.")
+                student_grades[name] = grade
+print(student_grades)
