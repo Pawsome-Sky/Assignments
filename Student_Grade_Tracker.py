28cfd439849a2361c04158ebcb61cdc285a52ac9
Pawsome-Sky <skaistediskiene@gmail.com>
2024-08-09 23:22:51 +0300

    Changed the code and added some of the bonuses.

--- Student_Grade_Tracker.py
+++ Student_Grade_Tracker.py
@@ -1,23 +1,53 @@
 print("Student Grade Tracker")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value != 0:
+                return value
+            else:
+                print("The number cannot be zero. Please enter a number that is not zero.")
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+
+def get_float(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid. Please enter a valid number.")
+
+
 student_grades = {
 
 }
-subjects = ["first", "second", "third"]
-num_students = int(input("Enter the number of students: "))
-i = 0
-while i < num_students:
+
+# Number of students and names
+num_students = get_int("Enter the number of students: ")
+for _ in range(num_students):
     name = input("Enter student name: ")
     student_grades[name] = {}
-    i += 1
-    for subject in subjects:
+    # Number of subjects and names
+    num_subjects = get_int(f"Enter the number of subjects {name} has: ")
+    for _ in range(num_subjects):
+        subject_name = input("Enter the name of the subject: ")
+        student_grades[name][subject_name] = {}
+        # Grades for each student for the subjects
         while True:
-            grade = int(input(f"Enter the grade {name} received in the {subject} subject: "))
+            grade = get_float(f"Please enter the grade {name} received for {subject_name}: ")
             if 0 <= grade <= 100:
-                student_grades[name][subject] = int(grade)
+                student_grades[name][subject_name] = int(grade)
                 break
             else:
                 print("Please enter a valid grade between 0 and 100.")
-    numbers = list(student_grades[name].values())
-    total_grades = sum(numbers)
-    average_grade = total_grades / 3
-    print(f"Average grade for {name}: {average_grade}")
+            # print(student_grades)
+# Calculate average student grade
+for name in student_grades:
+    grades = list(student_grades[name].values())
+    total_grades = sum(grades)
+    num_grades = len(grades)
+    average_grade = total_grades / num_grades if num_grades > 0 else 0
+    print(f"Average grade for {name}: {average_grade:.2f}")
