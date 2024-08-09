ef0da2c2eeacb8c3dbb3cc2369eaa73fb6bd5e09
Pawsome-Sky <skaistediskiene@gmail.com>
2024-08-09 19:04:40 +0300

    Completed task without adding bonuses

--- Student_Grade_Tracker.py
+++ Student_Grade_Tracker.py
@@ -1,20 +1,23 @@
 print("Student Grade Tracker")
-students = []
-student_grades = {}
+student_grades = {
+
+}
 subjects = ["first", "second", "third"]
 num_students = int(input("Enter the number of students: "))
 i = 0
 while i < num_students:
     name = input("Enter student name: ")
-    students.append(name)
+    student_grades[name] = {}
     i += 1
-    for student in students:
-        for subject in subjects:
-            while True:
-                grade = int(input(f"Enter the grade {student} received in the {subject} subject: "))
-                if 0 <= grade <= 100:
-                    break
-                else:
-                    print("Please enter a valid grade between 0 and 100.")
-                student_grades[name] = grade
-print(student_grades)
+    for subject in subjects:
+        while True:
+            grade = int(input(f"Enter the grade {name} received in the {subject} subject: "))
+            if 0 <= grade <= 100:
+                student_grades[name][subject] = int(grade)
+                break
+            else:
+                print("Please enter a valid grade between 0 and 100.")
+    numbers = list(student_grades[name].values())
+    total_grades = sum(numbers)
+    average_grade = total_grades / 3
+    print(f"Average grade for {name}: {average_grade}")
